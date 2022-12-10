607ec3820b5d23883d589cf0bbad2bdb732af483
sabrinaMac <sabrina.scherma@gmail.com>
2022-12-09 23:23:19 -0700

    worked on react app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/campsitesDirectoryPage';
+import { Routes, Route } from 'react-router-dom'
+import ContactPage from './pages/ContactPage'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='contact' element={<ContactPage />}/>
+        <Route path='directory' element={<CampsitesDirectoryPage />}/>
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,14 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { useState } from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavITem,
+  NavItem,
+} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 import NucampLogo from '../app/assets/img/logo.png';
 
 const Header = () => {
@@ -7,6 +17,28 @@ const Header = () => {
       <NavbarBrand href="/">
         <img src={NucampLogo} alt="nucamp logo" />
       </NavbarBrand>
+      <Nav className="ms-auto" navBar>
+        <NavItem>
+          <NavLink className="nav-link" to="/">
+
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="/directory">
+
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="/about">
+
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="contact">
+
+          </NavLink>
+        </NavItem>
+      </Nav>
     </Navbar>
   );
 };
