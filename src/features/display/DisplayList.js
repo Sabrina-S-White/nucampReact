f4bf4fd7398daaab8d354d2a3b5c13f95f19edfe
sabrinaMac <sabrina.scherma@gmail.com>
2022-12-31 10:53:53 -0700

    added error messages

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -4,24 +4,35 @@ import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 const DisplayList = () => {
   const items = useSelector((state) => [
-    selectFeaturedCampsite(state), selectFeaturedPromotion(state), selectFeaturedPartner(state)
+    selectFeaturedCampsite(state),
+    selectFeaturedPromotion(state),
+    selectFeaturedPartner(state),
   ]);
 
   return (
     <Row>
-    {items.map((item, idx) => {
+      {items.map((item, idx) => {
+        const { featuredItem, isLoading, errMsg } = item;
+        if (isLoading) {
+          return <Loading key={idx} />;
+        }
+        if (errMsg) {
+          return <Error errMsg={errMsg} key={idx} />;
+        }
         return (
-          item && (
-            <Col md className='m-1' key={idx}>
-                <AnimatedDisplayCard item={item} />
+          featuredItem && (
+            <Col md className="m-1" key={idx}>
+              <AnimatedDisplayCard item={featuredItem} />
             </Col>
           )
         );
-    })}
-</Row>
+      })}
+    </Row>
   );
 };
 
